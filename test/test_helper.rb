44c5f161808465c25b6b2fd371b74d794e5412cc
Tore Darell <toredarell@gmail.com>
2011-03-08 17:41:13 +0100

    Remove ActionView require.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require 'test/unit'
 require 'active_support'
 require 'active_support/test_case'
 require 'active_record'
-require 'action_view'
 require 'mocha'
 require 'translatable_attributes'
 $: << File.join(File.dirname(__FILE__), '..', 'app', 'models')
